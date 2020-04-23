90979fb57952d764c38a05104368655de8137edc
Georgy Filin <8740365@mail.ru>
2020-04-23 13:12:33 +0300

    Рефакторинг

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -17,60 +17,11 @@ namespace TestConsole
             Player player1 = new Player("Empty", new DateTime(1993, 04, 17));
 
             Console.Write("Введите фамилию >");
-            var surname = Console.ReadLine();
+            player1.Name = Console.ReadLine();
 
            Console.WriteLine(player1.Name);
 
             Console.ReadLine();
-        }
-    }
-
-    internal class Player
-    {
-        private string _Name;
-        private DateTime _Birthday;
-
-        public string GetName()
-        {
-            return _Name;
-        }
-
-        public string SetName(string Name)
-        {
-            _Name = Name;
-        }
-
-        public string Name
-        {
-            get
-            {
-                return _Name;
-            }
-            set
-            {
-                _Name = value;
-            }
-        }
-
-        public string Surname { get; set; } = "qwe";
-
-        public Player()
-        {
-            Surname = "123";
-        }
-
-        public Player(string Name)
-        {
-            this._Name = Name;
-            _Birthday = DateTime.Now;
-        }
-
-        public Player(string Name, DateTime Birthday)
-            :this(Name)        
-        {
-            //this.Name = Name;
-            this._Birthday = Birthday;
-
         }
     }
 
