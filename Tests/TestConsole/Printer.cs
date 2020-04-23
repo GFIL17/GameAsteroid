88516e46caffc8cd20a7fb35daa026bc99a35eee
Georgy Filin <8740365@mail.ru>
2020-04-23 14:18:30 +0300

    Пример наследования и полиморфизм

---
+++ Tests/TestConsole/Printer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Printer
+    {
+
+        public virtual void Print(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
