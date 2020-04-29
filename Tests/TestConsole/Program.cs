5267cb8c72ccb198a0b5e455bd2355c9264058f5
Georgy Filin <8740365@mail.ru>
2020-04-29 20:29:43 +0300

    Описание абстрактного класса Logger

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -11,8 +11,14 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            
+            //Logger log = new Logger();
+
+            //log.LogInformation("Message1");
+            //log.LogWarning("Info message");
+            //log.LogError("Error message");
+
+
             Console.ReadLine();
         }
-    }
+    }    
 }
