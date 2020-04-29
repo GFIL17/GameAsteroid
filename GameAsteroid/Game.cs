99fcb3ce670231d7fc666d157ec70fadc852a779
Georgy Filin <8740365@mail.ru>
2020-04-29 18:07:39 +0300

    Комментарии + рефакторинг

--- GameAsteroid/Game.cs
+++ GameAsteroid/Game.cs
@@ -8,17 +8,26 @@ using System.Drawing;
 
 namespace GameAsteroid
 {
+    /// <summary> Класс игровой логики </summary>
     internal static class Game
     {
+        /// <summary> Интервал времени таймера формирования кадра игры </summary>
+        private const int __TimerInterval = 100;
+
         private static BufferedGraphicsContext __Context;
         private static BufferedGraphics __Buffer;
 
         private static VisualObject[] __GameObjects;
 
-        public static int Width { get; set; }
+        /// <summary> Ширина игрового поля </summary>
+        public static int Width { get; private set; }
 
-        public static int Height { get; set; }
+        /// <summary> Высота игрового поля </summary>
+        public static int Height { get; private set; }
 
+
+        /// <summary> Инициализация игровой логики </summary>
+        /// <param name="form"> Игровая форма </param>
         public static void Initialize(Form form)
         {
             Width = form.Width;
@@ -28,7 +37,7 @@ namespace GameAsteroid
             Graphics g = form.CreateGraphics();
             __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
 
-            Timer timer = new Timer { Interval = 100 };
+            Timer timer = new Timer { Interval = __TimerInterval };
             timer.Tick += OnVimerTick;
             timer.Start();
 
@@ -53,7 +62,7 @@ namespace GameAsteroid
 
             __Buffer.Render();
         }
-     
+
         public static void Load()
         {
             __GameObjects = new VisualObject[30];
