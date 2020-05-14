8b67a98f66346d3b0e6eecd2e7d5177b59dcf682
Georgy Filin <8740365@mail.ru>
2020-05-14 12:27:54 +0300

    Модель данных

--- SamplesUI/StudentManager/Models/StudentsGroup.cs
+++ SamplesUI/StudentManager/Models/StudentsGroup.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace StudentManager.Models
 {
-    class StudentsGroup
+    internal class StudentsGroup
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
     }
 }
