7c77088da7e409d9556a4da85e91d90a548d3bbb
Georgy Filin <8740365@mail.ru>
2020-05-14 15:18:25 +0300

    Работа с моделями-представления

---
+++ SamplesUI/StudentManager/ViewModels/StudentGroupsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using StudentManager.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager.ViewModels
+{
+    class StudentGroupsViewModel : ViewModel
+    {
+
+    }
+
+    class StudentsViewModel : ViewModel
+    {
+
+    }
+}
