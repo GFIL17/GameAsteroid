b963e7ff0c71cdf1c944b90ac4d435aaf86b7bf3
Georgy Filin <8740365@mail.ru>
2020-05-17 19:12:43 +0300

    Строки подключения в БД

--- DataBase/AdoNetTest/Program.cs
+++ DataBase/AdoNetTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace AdoNetTest
 {
@@ -10,6 +12,18 @@ namespace AdoNetTest
     {
         static void Main(string[] args)
         {
+            var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            //DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
+            //var new_connectionstring = builder.ConnectionString;
+
+            var sql_server_connection_string_builder = new SqlConnectionStringBuilder(connection_string);
+            sql_server_connection_string_builder.IntegratedSecurity = false;
+            sql_server_connection_string_builder.UserID = "User";
+            sql_server_connection_string_builder.Password = "Password";
+            sql_server_connection_string_builder.InitialCatalog = "TestDB123";
+
+            var new_connection_string = sql_server_connection_string_builder.ConnectionString;
         }
     }
 }
