de4e0923ea63e80dcc23f1a8dbeacc02a7042469
Georgy Filin <8740365@mail.ru>
2020-05-18 14:48:58 +0300

    Контекст для подключения к БД

---
+++ DataBase/StudentsDatabaseTest/Data/StudentsDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using StudentsDatabaseTest.Data.Entityes;
+
+namespace StudentsDatabaseTest.Data
+{
+    public class StudentsDB:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentsGroup> Groups { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        //public StudentsDB() : this("StudentsDB") { }
+
+        public StudentsDB() : this("name=StudentsDB") { }
+
+        public StudentsDB(string ConnectionString) : base(ConnectionString) { }
+    }
+}
