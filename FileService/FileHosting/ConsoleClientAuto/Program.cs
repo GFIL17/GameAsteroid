aa66c8f406f3a6b6a327761073d922fac528703b
Georgy Filin <8740365@mail.ru>
2020-05-19 21:03:18 +0300

    Автоматически генерируемый клиент

---
+++ FileService/FileHosting/ConsoleClientAuto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleClientAuto.FileService;
+
+namespace ConsoleClientAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new FileServiceClient();
+
+            var files = client.GetFiles("C:\\Users\\Georgy\\Desktop\\Games");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+
+            client.StartProcess("calc", "");
+
+            Console.ReadLine();
+        }
+    }
+}
